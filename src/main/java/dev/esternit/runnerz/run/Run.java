b46a1715399edbfcdee2be8c3686ac061949a525
Esternit <92298236+Esternit@users.noreply.github.com>
2024-06-24 20:34:35 +0300

    some validation added

--- src/main/java/dev/esternit/runnerz/run/Run.java
+++ src/main/java/dev/esternit/runnerz/run/Run.java
@@ -1,7 +1,23 @@
 package dev.esternit.runnerz.run;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
 import java.time.LocalDateTime;
 
-public record Run(Integer id, String title, LocalDateTime startTime, LocalDateTime endTime, Integer miles, Location location) {
+public record Run(Integer id,
+                  @NotEmpty
+                  String title,
+                  LocalDateTime startTime,
+                  LocalDateTime endTime,
+                  @Positive
+                  Integer miles,
+                  Location location
+) {
 
+    public Run {
+        if(!endTime.isAfter(startTime)){
+            throw new IllegalArgumentException("End time must be after start time");
+        }
+    }
 }
